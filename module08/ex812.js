0dc3a61eb5d43fca71176c370a7ce5bcc8a48a1e
Your Name <abdalrahamn.abd@gmail.com>
2018-11-29 11:15:53 +0200

    Initial Site Setup

---
+++ module08/ex812.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(800, 500);
+  background('#31bc33');
+
+}
+
+function draw() {
+
+}
--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+  console.log("hello p5");
 }
 
 function draw() {
